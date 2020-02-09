6fc6ef6ba7c16f685c63c2e6520f6ef15a2a3bf1
HT-online <60575533+HT-online@users.noreply.github.com>
2020-02-09 18:29:01 +0300

    Update HTcode.js

--- HTcode.js
+++ HTcode.js
@@ -299,7 +299,7 @@ window.onload = function () {
 			  }
       
 			for(let a=0;a<5;a++){
-				console.log(a+' ');
+				console.log('a = '+a);
 				for(let k=0;k<5;k++)waterGeometryChunks[a,k] = new THREE.Geometry();
 				for(let b=0;b<5;b++){
           
@@ -362,9 +362,8 @@ window.onload = function () {
       		}
       		waterGeometryChunks[a,b].computeFaceNormals();
 					waterMeshChunks[a,b].geometry = waterGeometryChunks[a,b];
-          console.log(b+' ');
+          console.log('b = '+b);
 				}
-        console.log('\n');
 			}
       
 			
