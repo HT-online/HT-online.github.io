e0869740d35b4e6d280c0825cd6b26ca9b67c425
HT-online <60575533+HT-online@users.noreply.github.com>
2020-02-13 00:33:05 +0300

    Update HTcode.js

--- HTcode.js
+++ HTcode.js
@@ -309,7 +309,7 @@ window.onload = function () {
       lastheight1 = Math.sin((incX-1)/sinMulti);
 			for(let a=0;a<waterMeshChunks.length;a++){
         lastheight2 = Math.sin((a*iMax-1+incZ)/sinMulti);
-        console.log('a '+a+' lh2 '+lastheight2+' BEFORE');
+        //console.log('a '+a+' lh2 '+lastheight2+' BEFORE');
 				for(let b=0;b<waterMeshChunks.length;b++){
           lastheight1 = Math.sin((b*jMax-1+incX)/sinMulti);
           
@@ -318,6 +318,8 @@ window.onload = function () {
         		for(let j=0; j<jMax; j++){
 				      height1 = Math.sin((b*jMax+j+incX)/sinMulti);
               
+              if(a==0&&b==0&&i==0&&j==9)console.log(height2+' DO');
+              if(a==0&&b==1&&i==0&&j==0)console.log(lastheight2+' POSLE');
             	if(i*polygonSize - shipPosition.x - 30*Math.sin(shipRotation.y) < polygonSize && j*polygonSize - shipPosition.z - 30*Math.cos(shipRotation.y) < polygonSize){
               	y1 = lastheight1*lastheight2*waveMulti;
 								CUBE1.position.x = i*polygonSize + 30*Math.sin(shipRotation.y);
@@ -330,8 +332,7 @@ window.onload = function () {
 								CUBE2.position.y = y2;
 								CUBE2.position.z = j*polygonSize - 30*Math.cos(shipRotation.y);
             	}
-              if(a==0&&i==0)lastheight2=-1;
-              console.log('a '+a+' lh2 '+lastheight2+' AFTER');
+              //if(a==0&&i==0)lastheight2=a*iMax-1+incZ;
           		waterGeometryChunks[a][b].vertices.push(
             		new THREE.Vector3(polygonSize*(j+b*jMax), lastheight1*lastheight2*waveMulti, polygonSize*(i+a*iMax)),
             		new THREE.Vector3(polygonSize*(j+b*jMax), height2*lastheight1*waveMulti, polygonSize*(i+1+a*iMax)),
